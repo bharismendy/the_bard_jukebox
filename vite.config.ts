59343d00ef389340ea05f7cef74c3333ba5b0d71
Brice Harismendy <briceharismendy@gmail.com>
2025-02-09 18:14:48 +0100

    update workflow

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import vueJsx from "@vitejs/plugin-vue-jsx";
 import vueDevTools from "vite-plugin-vue-devtools";
 
 export default defineConfig({
+  base: "/rpg_sound_booard/",
   plugins: [vue(), vueJsx(), vueDevTools()],
   css: {
     preprocessorOptions: {
